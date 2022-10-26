f72b933bd0348e5cc439a13eea9fe1e870ea8a05
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-26 11:18:39 +0530

    Create possible_outcomes_when_2_dices_are_rolled.c

---
+++ possible_outcomes_when_2_dices_are_rolled.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+    for(int i=1;i<=6;i++)//i is for rows 
+{
+
+    for (int j=1;j<=6;j++)//j is for columns
+    {
+    
+        printf("(%d,%d)\t",i,j);
+        
+    }
+   
+    printf("\n");
+}
+}
+    
+
