3a30e0a9723a16b2706ac71c9a96a0a96381462a
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-26 11:07:47 +0530

    Create print_1_to_5_5_times.c

---
+++ print_1_to_5_5_times.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+    for(int i=5;i>0;i--)//i is for rows 
+{
+
+    for (int j=1;j<=5;j++)//j is for columns
+    {
+    
+        printf("%d\t",j);
+        
+    }
+    printf("\n");
+}
+}
+    
+
