685326680daa8408affd02a1a934020c5964d5c7
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-26 09:43:35 +0530

    Create half_triangle.c

---
+++ half_triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+    for(int i=1;i<5;i++)
+{
+
+    for (int j=1;j<=i;j++)
+    {
+    
+        printf("%d\t",j);
+        
+    }
+    printf("\n");
+}
+}
+    
