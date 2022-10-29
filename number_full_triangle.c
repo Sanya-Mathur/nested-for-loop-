375f39fa1ab3ee2daea0d003d3b624a71f1a84bf
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-29 09:10:08 +0530

    Create number_full_triangle.c

---
+++ number_full_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+int number=1;
+ int count =3;
+    for(int r=1;r<=3;r++)//i is for rows 
+{
+int star=0;
+
+    for (int spaces=1;spaces<=5;spaces++)//j is for columns
+    {
+    if (spaces<count)
+    {
+        printf(" ");
+    }
+    if ((spaces>=count)&&(star<(2*r-1)))
+    {    printf("%d",number );// prints numbers successively 
+        number ++;
+        star++;
+    }
+   
+    }
+    printf("\n");
+      count--;
+   
+  
+}
+}
