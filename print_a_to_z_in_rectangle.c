83688afc869aa7c0816fea8c5d6cdf573c29a1a4
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-31 19:05:33 +0530

    Create print_a_to_z_in_rectangle.c

---
+++ print_a_to_z_in_rectangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() 
+{   
+    printf("RA2211042010042\n");
+     char character='a';
+    for(int i=1;i<3;i++)//i is for rows 
+{
+   
+    for (int j=1;j<14;j++)// j is for columns 
+    {
+      
+        printf("%c\t",character);
+          character++;
+        
+    }
+    printf("\n");
+}
+}
+    
