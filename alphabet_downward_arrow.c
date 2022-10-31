004d29f6f68340efe598b1323ff4640dc048fca3
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-31 21:20:42 +0530

    Create alphabet_downward_arrow.c

---
+++ alphabet_downward_arrow.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+char c='a';
+int blankspaces;int r=4;
+ for (int row=2;row<=5;row++)//rows 
+ {   blankspaces=0;
+ 
+    for (int spaces=1;spaces<8;spaces++)//positions in a line
+    {
+        if (spaces==(row-1))
+        {printf("%c",c);
+        c++;
+              
+        }
+        else if (spaces<(row-1))
+        {printf(" ");
+        blankspaces++;
+              
+        }
+        
+       else if (spaces==(blankspaces+(2*r-1)))
+       { printf("%c",c);
+        c++;
+            
+       }
+        else if (spaces>(row-1))
+        {printf(" ");
+       
+        }
+        
+    }
+    printf("\n");
+    r--;
+ }
+}
