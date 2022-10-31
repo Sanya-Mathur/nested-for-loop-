c03a1ce856a46950c015e612e71be32c0656dbdc
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-31 23:43:39 +0530

    Create hollow_star_rectangle_line.c

---
+++ hollow_star_rectangle_line.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+for(int iter=1;iter<=10;iter++)//iter=iterations
+{for(int i=1;i<=10;i++)
+{
+  printf("*")  ;
+}
+printf("\n")  ;
+for(int rows=1;rows<=10;rows++)
+{for(int j=1;j<=10;j++)
+{
+    if(j==1||j==10)
+    {
+        printf("*");
+    }
+    else
+    printf(" ");
+}
+printf("\n")  ;
+}
+for(int k=1;k<=10;k++)
+{
+  printf("*")  ;
+}
+printf("\n");
+}
+}
