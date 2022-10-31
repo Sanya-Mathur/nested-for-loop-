9cd934161be2155784b37159b26c9f8ee3b2cc24
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-31 23:23:37 +0530

    Create diamond_lines.c

---
+++ diamond_lines.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void main() 
+{  printf("RA2211042010042\n");
+int n; 
+printf("enter number of diamonds");
+scanf("%d",n);
+int ublankspaces,ur,dblankspaces,dr;
+for(int k=1;k<=n;k++)
+{  ur=1;
+ for (int urow=5;urow>=2;urow--)//for rows
+ {   ublankspaces=0;
+ 
+    for (int uspaces=1;uspaces<8;uspaces++)//for positions in one line 
+    {
+        if (uspaces==(urow-1))
+        {printf("*");
+              
+        }
+        else if (uspaces<(urow-1))
+        {printf(" ");
+        ublankspaces++;
+              
+        }
+        
+       else if (uspaces==(ublankspaces+(2*ur-1)))
+       { printf("*");
+            
+       }
+        else if (uspaces>(urow-1))
+        {printf(" ");
+       
+        }
+        
+    }
+    printf("\n");
+    ur++;
+ }
+ 
+dr=3;
+ for (int drow=3;drow<=5;drow++)//rows 
+ {   dblankspaces=0;
+ 
+    for (int dspaces=1;dspaces<8;dspaces++)//positions in a line
+    {
+        if (dspaces==(drow-1))
+        {printf("*");
+              
+        }
+        else if (dspaces<(drow-1))
+        {printf(" ");
+        dblankspaces++;
+              
+        }
+        
+       else if (dspaces==(dblankspaces+(2*dr-1)))
+       { printf("*");
+            
+       }
+        else if (dspaces>(drow-1))
+        {printf(" ");
+       
+        }
+        
+    }
+    printf("\n");
+    dr--;
+ }
+ }
+}
