a950673797ef495eea8130c3b01b3b8e75d24440
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-10-31 19:02:32 +0530

    Create print_position_numbers_4by4_square.c

---
+++ print_position_numbers_4by4_square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() 
+{   
+    printf("RA2211042010042\n");
+     int count=0;
+    for(int i=1;i<5;i++)//i is for rows 
+{
+   
+    for (int j=1;j<5;j++)// j is for columns 
+    {
+      
+        printf("%d\t",count);
+          count++;
+        
+    }
+    printf("\n");
+}
+}
+    
